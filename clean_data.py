7de09e5592be045bc59651331fbee2a096670c3c
Anna Kryvous <elyssray@users.noreply.github.com>
2017-08-09 17:26:10 +0200

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -1,29 +1,92 @@
-# -*- coding: utf-8 -*-
-import os, re
-import pandas as pd
-
-wd = 'c:/Users/Alonso/Desktop/TMProject'
+import os, re, io
+wd = 'C:\Users\Ray\Desktop\TMProject'
 os.chdir(wd)
+import pandas as pd
+import numpy as np
 
-import textminer as tm
+## FOR MULTIPLE FILES IN A FOLDER
 
-## files = tm.read_dir_txt('c:/Users/Alonso/Desktop/TMProject/COHA/')
+filepath = 'C:\Users\Ray\Desktop\TMProject\COCA'
+filenames = os.listdir(filepath)
+text_list = []
+for filename in filenames:
+    tmp = filepath + '\\' + filename
+    with io.open(tmp, 'r', encoding = 'utf-8') as f:
+        text = f.read()
+    text_list.append(text)
 
-def read_directory_dic(directory):
-    """
-    Import multiple txt files from a directory in dictionary form
-    """
-    filenames = os.listdir(directory)
-    result_list = {}
-    for txt in filenames:
-        filepath = directory + txt
-        text = tm.read_txt(filepath)
-        f = {txt: text}
-        result_list.update(f)
-    return(result_list)
+df = pd.DataFrame()
+df['filename'] = filenames
+df['text'] = text_list
 
-files = read_directory('c:/Users/Alonso/Desktop/TMProject/COHA/')
+# genre column
+genre = []
+for text in filenames:
+    text = re.sub(r'[^a-zA-Z]','',text)
+    text = text.rstrip()
+    text = re.sub(r'[txt]','',text)
+    text = text[1:5]
+    genre.append(text)
+df['genre'] = genre
 
-### df - create a data frame with pandas
+# year column
+year = []
+for text in filenames:
+    text = re.sub(r'[a-zA-Z]','',text)
+    text = text[2:6]
+    year.append(text)
+df['year'] = year
 
-df = pd.DataFrame({'file_name': files.keys(), 'content': files.values()})
+## FOR SINGLE FILE (CSV)
+import csv
+source = open('C:\\Users\\Ray\\Desktop\\TMProject\\corpus_blogs.csv')
+blogs = list(csv.reader(source))
+
+df = pd.DataFrame()
+df['text'] = blogs
+
+## CLEAN - TOKENIZE - NOSTOP
+
+# clean text (also without commas or dots)
+textClean = []
+for text in df['text']:
+    text = re.sub(r'[^a-zA-Z]',' ',text)
+    text = re.sub(r' +',' ', text)
+    text = text.rstrip()
+    textClean.append(text)
+df['clean_text'] = textClean
+
+# tokenize
+def tokenize(input, length = 0, casefold = False):
+    tokenizer = re.compile(r'[^A-Za-z]+')
+    if casefold:
+        input = input.lower()
+    tokens = [token for token in tokenizer.split(input) if len(token) > length]
+    return tokens
+
+token_text = [tokenize(i, 1, True) for i in textClean]
+df['token_text'] = token_text
+
+# generate stopword list from unsliced text
+from collections import defaultdict
+from operator import itemgetter
+
+def gen_ls_stoplist(input, n = 100):
+    t_f_total = defaultdict(int)
+    #n = 100
+    for text in input:
+        for token in text:
+            t_f_total[token] += 1
+    nmax = sorted( t_f_total.items(), key = itemgetter(1), reverse = True)[:n]
+    return [elem[0] for elem in nmax]
+sw = gen_ls_stoplist(token_text, 150)
+
+# apply stopword list
+token_nostop = []
+no_sw = []
+
+for fragment in token_text:
+    no_sw = [token for token in fragment if token not in sw]
+    token_nostop.append(no_sw)
+    
+df['token_nostop'] = token_nostop
